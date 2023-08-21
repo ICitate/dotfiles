7fbeeef15ef9ef4305b030c6579848fa12880e9f
ICitate <nachocitate@gmail.com>
2023-08-21 19:59:55 -0300

    telescope config and colorscheme simplification

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,16 +1,1 @@
-require("tokyonight").setup({
-  style = "dark", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  transparent = false, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-})
-
-function ColorMyPencils(color)
-	color = color or "tokyonight-night"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-end
-
-ColorMyPencils()
+vim.cmd.colorscheme("tokyonight-night")
--- nvim/.config/nvim/lua/nacho/packer.lua
+++ nvim/.config/nvim/lua/nacho/packer.lua
@@ -1,24 +1,15 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-   use({
-	  'folke/tokyonight.nvim',
-	  as = 'tokyonight',
-	  config = function()
-		  vim.cmd('colorscheme tokyonight')
-	  end
-    })
+    use({ 'folke/tokyonight.nvim', as = 'tokyonight' })
 
     use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
 
